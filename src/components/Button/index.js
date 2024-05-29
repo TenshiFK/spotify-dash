2e13a0b79a1cda58dba5030e886317a575c80abe
TenshiFK <124812869+TenshiFK@users.noreply.github.com>
2024-05-29 20:52:04 -0300

    Update index.js

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoginButton(){
 
     const SPOTIFY_CLIENT_ID = '3a358ff835a04f228121326266278a6d';
-    const REDIRECT_URI = 'https://projeto-spotify-test.vercel.app/dashboard';
+    const REDIRECT_URI = 'https://spotify-dash-di.vercel.app/dashboard';
     const SCOPE = 'user-top-read%20user-read-recently-played%20playlist-read-private%20playlist-read-collaborative%20user-read-email%20user-read-private';
 
     return(
